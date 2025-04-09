59e2b9666ee8d279e4e89149ce4349b3ab2a60aa
Shreyashthaware2003 <shreyashthaware284@gmail.com>
2025-04-09 15:04:29 +0530

    did some changes for deployement

--- backend/controller/admin.controller.js
+++ backend/controller/admin.controller.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { Admin } from "../model/admin.model.js";
 
 export const registerAdmin = async (req, res) => {
--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 import { User } from "../model/user.model.js";
 
 const generateToken = (userId) => {
